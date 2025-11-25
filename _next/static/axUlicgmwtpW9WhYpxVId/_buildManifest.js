2243de53b874958ae239782f57389a53e8f74c46
RagavaRvd <ragavarvd@gmail.com>
2025-11-25 19:49:17 +0530

    Updates

--- _next/static/axUlicgmwtpW9WhYpxVId/_buildManifest.js
+++ _next/static/axUlicgmwtpW9WhYpxVId/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/ragav-portfolio//_next/:path+",
-        "destination": "/ragav-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
